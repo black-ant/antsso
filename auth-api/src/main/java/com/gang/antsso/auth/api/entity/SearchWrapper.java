2f358ac3f7b0f75aaea14d8946043fb6335c0aba
black-ant <1016930479@qq.com>
2020-06-25 23:44:31 +0800

    add

---
+++ auth-api/src/main/java/com/gang/antsso/auth/api/entity/SearchWrapper.java
@@ -0,0 +1,10 @@
+package com.gang.antsso.auth.api.entity;
+
+/**
+ * @Classname SearchWrapper
+ * @Description TODO
+ * @Date 2020/6/25 23:44
+ * @Created by zengzg
+ */
+public class SearchWrapper {
+}
