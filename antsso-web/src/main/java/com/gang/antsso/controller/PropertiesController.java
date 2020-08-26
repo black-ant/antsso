f96a5b0447d31f85b6450a59a9c895131fa19868
black-ant <1016930479@qq.com>
2020-08-26 22:46:05 +0800

    add

---
+++ antsso-web/src/main/java/com/gang/antsso/controller/PropertiesController.java
@@ -0,0 +1,10 @@
+package com.gang.antsso.controller;
+
+/**
+ * @Classname PropertiesController
+ * @Description TODO
+ * @Date 2020/8/26 22:15
+ * @Created by zengzg
+ */
+public class PropertiesController {
+}
