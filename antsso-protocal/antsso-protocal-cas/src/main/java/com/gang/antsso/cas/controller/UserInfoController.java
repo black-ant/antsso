2d66bc2a50a5d3e37d916b2ab2e2d7d44864b970
black-ant <1016930479@qq.com>
2020-08-30 22:27:11 +0800

    renew

---
+++ antsso-protocal/antsso-protocal-cas/src/main/java/com/gang/antsso/cas/controller/UserInfoController.java
@@ -0,0 +1,10 @@
+package com.gang.antsso.cas.controller;
+
+/**
+ * @Classname UserInfoController
+ * @Description TODO
+ * @Date 2020/8/30 17:25
+ * @Created by zengzg
+ */
+public class UserInfoController {
+}
